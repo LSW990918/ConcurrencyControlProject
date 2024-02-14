aa11e9aadf6984d771f1eadb0fdc2f4655c3c61d
seunghwan <lovelyunsh@naver.com>
2024-02-14 17:34:34 +0900

    #9 Style : goods Entity column명 지정, data class -> class

--- src/main/kotlin/com/a03/concurrencycontrolproject/domain/goods/model/goods.kt
+++ src/main/kotlin/com/a03/concurrencycontrolproject/domain/goods/model/goods.kt
@@ -9,22 +9,30 @@ import java.time.LocalDateTime
 @Entity
 @SQLDelete(sql = "Update goods SET is_deleted = true WHERE id = ?")
 @Table(name = "goods")
-data class goods(
+class goods(
+    @Column(name = "title")
     val title: String,
+    @Column(name = "running_time")
     val runningTime: Int,
+    @Column(name = "date")
     val date: LocalDateTime,
+    @Column(name = "ticket_amount")
     val ticketAmount: Int,
+    @Column(name = "price")
     val price: Int,
+    @Column(name = "place")
     val place: String,
     //@ManyToOne
     // val category:Category,
     //@ManyToOne
     // val user:User
-    var isDeleted: Boolean = false,
+    @Column(name = "is_deleted")
+    val isDeleted: Boolean = false,
+    @Column(name = "bookable_date")
     val bookableDate: LocalDateTime
 ) : BaseTime() {
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
-
 }
