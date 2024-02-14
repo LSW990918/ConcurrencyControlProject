32fbce192e25177d0b71aa5554c4d376e2f95724
Miso <un5754@naver.com>
2024-02-14 17:39:05 +0900

    #11 Feat: Review entity 생성

---
+++ src/main/kotlin/com/a03/concurrencycontrolproject/domain/review/Review.kt
@@ -0,0 +1,34 @@
+package com.a03.concurrencycontrolproject.domain.review
+
+import com.a03.concurrencycontrolproject.common.BaseTime
+import jakarta.persistence.*
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
+
+@Entity
+@Table(name = "review")
+class Review(
+    @Column(name = "comment")
+    var comment: String,
+
+    @Column(name = "score")
+    var score: Int,
+
+    @Column(name = "is_deleted")
+    var isDeleted: Boolean,
+
+//    @ManyToOne
+//    @OnDelete(action = OnDeleteAction.CASCADE)
+//    @JoinColumn(name = "user_id")
+//    val user: User,
+
+/*    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "goods_id")*/
+//    val goods: Goods
+):BaseTime() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    var id: Long? = null
+}
