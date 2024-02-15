fd558ca5abb81f82c4a9c9a72191c91b56e62e2e
LSW990918 <leesywon99@gmail.com>
2024-02-15 15:31:03 +0900

    Ticket 초안 작성

--- src/main/kotlin/com/a03/concurrencycontrolproject/domain/ticket/controller/TicketController.kt
+++ src/main/kotlin/com/a03/concurrencycontrolproject/domain/ticket/controller/TicketController.kt
@@ -1,12 +1,10 @@
 package com.a03.concurrencycontrolproject.domain.ticket.controller
 
 import com.a03.concurrencycontrolproject.domain.ticket.dto.CreateTicketRequest
-import com.a03.concurrencycontrolproject.domain.ticket.service.TicketService
+import com.a03.concurrencycontrolproject.domain.ticket.dto.TicketResponse
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/tickets")
@@ -18,6 +16,27 @@ class TicketController(
     fun createTicket(
         @RequestBody request: CreateTicketRequest
     ): ResponseEntity<Unit> {
+        TODO()
+    }
+
+    @DeleteMapping("/{ticketId}")
+    fun deleteTicket(
+        @PathVariable ticketInt: Long
+    ): ResponseEntity<Unit> {
+        TODO()
+    }
+
+    @GetMapping()//Path로 유저아이디를 받아야함 -> 토큰에서 받아올 방법 생각해보기
+    fun getTicketOfMember(
+//        @AuthenticationPrincipal user: UserPrincipal
+    ): ResponseEntity<List<TicketResponse>> {
+        TODO()
+    }
+
+    @GetMapping("/{goodsId}")
+    fun getTicketOfGoods(
+        @PathVariable goodsId: Long
+    ): ResponseEntity<List<TicketResponse>> {
         TODO()
     }
 }
