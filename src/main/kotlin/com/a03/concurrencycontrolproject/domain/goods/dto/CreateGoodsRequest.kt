8dc2b829e61fa95de62156eacb28dd98a1a98fc4
seunghwan <lovelyunsh@naver.com>
2024-02-16 10:23:50 +0900

    #19 Fix : Dto validation 어노테이션 수정

--- src/main/kotlin/com/a03/concurrencycontrolproject/domain/goods/dto/CreateGoodsRequest.kt
+++ src/main/kotlin/com/a03/concurrencycontrolproject/domain/goods/dto/CreateGoodsRequest.kt
@@ -9,12 +9,12 @@ data class CreateGoodsRequest(
     val title: String,
     val place: String,
     val runningTime: Int,
-    @Pattern(
+    @field: Pattern(
         regexp = "\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]) (0[1-9]|1[0-9]|2[0-3]):(0[1-9]|[1-5][0-9]):(0[1-9]|[1-5][0-9])",
         message = "Please match date format yyyy-MM-dd HH:mm:ss"
     )
     private val date: String,
-    @Pattern(
+    @field: Pattern(
         regexp = "\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]) (0[1-9]|1[0-9]|2[0-3]):(0[1-9]|[1-5][0-9]):(0[1-9]|[1-5][0-9])",
         message = "Please match date format yyyy-MM-dd HH:mm:ss"
     )
