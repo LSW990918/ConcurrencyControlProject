bc9f0538e6cfabb53268b570a18ed911c4f8bdae
이승상 <soang2533@gmail.com>
2024-02-16 15:56:21 +0900

    #37 Refactor : toResponse -> dto로 옮기고 from으로 변경

--- src/main/kotlin/com/a03/concurrencycontrolproject/domain/user/dto/UserResponse.kt
+++ src/main/kotlin/com/a03/concurrencycontrolproject/domain/user/dto/UserResponse.kt
@@ -1,8 +1,21 @@
 package com.a03.concurrencycontrolproject.domain.user.dto
 
+import com.a03.concurrencycontrolproject.domain.user.model.User
+
 data class UserResponse (
     val id: Long,
     val email: String,
     val nickname: String,
     val role: String,
-)
+) {
+    companion object {
+        fun from(user: User): UserResponse {
+            return UserResponse(
+                id = user.id!!,
+                email = user.email,
+                nickname = user.nickname,
+                role = user.role.name
+            )
+        }
+    }
+}
