5fb9590c46ef1244f4daaef4bc009f13ad4d3f3f
LSW990918 <leesywon99@gmail.com>
2024-02-16 14:42:21 +0900

    #22 TicketResponse 수정 id -> Entity

--- src/main/kotlin/com/a03/concurrencycontrolproject/domain/ticket/dto/TicketResponse.kt
+++ src/main/kotlin/com/a03/concurrencycontrolproject/domain/ticket/dto/TicketResponse.kt
@@ -1,18 +1,20 @@
 package com.a03.concurrencycontrolproject.domain.ticket.dto
 
+import com.a03.concurrencycontrolproject.domain.goods.model.Goods
 import com.a03.concurrencycontrolproject.domain.ticket.model.Ticket
+import com.a03.concurrencycontrolproject.domain.user.model.User
 
 class TicketResponse(
     val id: Long,
-    val goodsId: Long,
-    val userId: Long,
+    val goods: Goods,
+    val user: User,
 ) {
     companion object {
         fun Ticket.toResponse(): TicketResponse {
             return TicketResponse(
                 id = id!!,
-                goodsId = goods.id!!,
-                userId = user.id!!
+                goods = goods,
+                user = user
             )
         }
     }
